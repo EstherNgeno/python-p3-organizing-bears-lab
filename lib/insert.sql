9e1ad1e48e7af0e28e956dfb6f73e471de67f430
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-10-18 15:14:28 +0300

    final commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 3, 'M', 'Black', 'Friendly', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 5, 'M', 'Orange', 'Playful', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 8, 'F', 'White', 'Calm', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 7, 'M', 'Brown', 'Brave', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 6, 'F', 'Black', 'Affectionate', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 2, 'M', 'White', 'Grumpy', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 1, 'F', 'Brown', 'Curious', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, NULL, NULL, NULL, NULL, True);
+
